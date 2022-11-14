a3fcf781ca4cb87d76d5faaf38082e65a69354ce
Brennan Colberg <brennan.colberg@gmail.com>
2022-11-13 23:53:14 -0800

    less beautiful but more compact listener init

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,7 @@ const input = document.getElementById("input") as HTMLInputElement;
 const output = document.getElementById("output") as HTMLParagraphElement;
 console.log({ input, output });
 
-input.addEventListener("change", onInput);
-input.addEventListener("keyup", onInput);
-function onInput() {
+input.onchange = input.onkeyup = () => {
 	console.log("update!");
 	output.textContent = input.value;
-}
+};
