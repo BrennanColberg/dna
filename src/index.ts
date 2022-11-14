94d592a046a2af79cc1f197fcb019d683b940e48
Brennan Colberg <brennan.colberg@gmail.com>
2022-11-13 23:51:08 -0800

    basic input -> output listener

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-alert("hello from ts");
+const input = document.getElementById("input") as HTMLInputElement;
+const output = document.getElementById("output") as HTMLParagraphElement;
+console.log({ input, output });
+
+input.addEventListener("change", onInput);
+input.addEventListener("keyup", onInput);
+function onInput() {
+	console.log("update!");
+	output.textContent = input.value;
+}
