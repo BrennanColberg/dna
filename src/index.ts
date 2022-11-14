71614aa57ef6f14a99cecbaf2728bc40a80d4b5a
Brennan Colberg <brennan.colberg@gmail.com>
2022-11-14 07:59:09 -0800

    actual dna translation

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,18 @@ console.log({ input, output });
 
 input.onchange = input.onkeyup = () => {
 	console.log("update!");
-	output.textContent = input.value;
+	const text = input.value;
+	const chars: number[] = [];
+	for (let i = 0; i < text.length; i++) {
+		if (text.charCodeAt(i) < 256) {
+			chars.push(text.charCodeAt(i));
+		}
+	}
+	output.textContent = chars
+		.map((char) => ("0000" + char.toString(4)).slice(-4))
+		.join(" ")
+		.replaceAll("0", "A")
+		.replaceAll("1", "C")
+		.replaceAll("2", "G")
+		.replaceAll("3", "T");
 };
